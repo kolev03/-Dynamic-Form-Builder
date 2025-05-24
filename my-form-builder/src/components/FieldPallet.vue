04458db14d41660836aebcf3a48fc7d20347c7d7
Petar Kolev <latinska@abv.bg>
2025-05-24 16:01:23 +0300

    Making the buttons

--- my-form-builder/src/components/FieldPallet.vue
+++ my-form-builder/src/components/FieldPallet.vue
@@ -1,9 +1,42 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+const buttons = ref([
+  { label: "Text Input", type: "text" },
+  { label: "Date Picker", type: "date" },
+  { label: "Dropdown", type: "select" },
+]);
+
+function addField(params) {
+  console.log(params);
+}
+</script>
 
 <template>
   <div class="main-column">
     <h1>Field Pallet</h1>
+    <div class="buttons">
+      <button
+        v-for="btn in buttons"
+        :key="btn.type"
+        @click="addField(btn.type)"
+      >
+        {{ btn.label }}
+      </button>
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.buttons {
+  display: flex;
+  width: 80%;
+  margin: auto;
+  flex-direction: column;
+  gap: 10px;
+}
+
+button {
+  font-size: 1.25rem;
+}
+</style>
